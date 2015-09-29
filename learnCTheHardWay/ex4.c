7b612f4d23f7b44520ea2025d29143dc66be074d
tykowale <tykowale@gmail.com>
2015-09-28 20:02:13 -0500

    example 4

---
+++ learnCTheHardWay/ex4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* Warning: This program is intentionally incorrect */ 
+
+// int main() 
+// {
+//   int age = 10;
+//   int height;
+
+//   printf("I am %d years old.\n");
+//   printf("I am %d inches tall.\n", height);
+
+//   return 0;
+// }
+
+/* fixed version */
+
+int main() 
+{
+  int age = 24;
+  int height = 70;
+
+  printf("I am %d years old.\n", age);
+  printf("I am %d inches tall.\n", height);
+
+  return 0;
+}
