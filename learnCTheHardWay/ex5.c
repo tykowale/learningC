a0a4680061b1e368ab5df6f440576fda5c3ea470
tykowale <tykowale@gmail.com>
2015-09-29 21:50:48 -0500

    example 5

---
+++ learnCTheHardWay/ex5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+
+  // another way to do a comment!
+  printf("You are %d miles away.\n", distance);
+  printf("Number of arguments = %d.\n", argc);
+
+  return distance;
+}
