8555566df8e991322e803a15c3b6861f8c23adbc
tykowale <tykowale@gmail.com>
2015-09-29 22:22:34 -0500

    length vs size in array

--- learnCTheHardWay/printArray.c
+++ learnCTheHardWay/printArray.c
@@ -3,10 +3,14 @@
 int main(int argc, char *argv[])
 {
   int foo[] = {1,2,3,4};
+  int sizeOfFoo = sizeof(foo);
+  int lengthOfFoo = sizeof(foo) / sizeof(foo[0]);
   int i;
-  for(i=0; i < (sizeof(foo) / sizeof(foo[0])); i++) {
+  for(i=0; i < lengthOfFoo; i++) {
     printf("%d \n", foo[i]);
   }
 
+  printf("size of foo: %d\n", sizeOfFoo);
+  printf("length of foo: %d\n", lengthOfFoo);
   return 0;
 }
