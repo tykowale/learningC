bf575e7c5ca958ad4cbb365af061dd1b6cd97b6c
tykowale <tykowale@gmail.com>
2015-11-04 19:43:48 -0600

    example 5.1

---
+++ ProgrammingInC/chapter5/ex1.c
@@ -0,0 +1,13 @@
+/*Program to calculate the eightn triangular number*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  int triangularNumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+
+  printf("The eight triangular number is %i\n", triangularNumber);
+
+  return 0;
+}
+
