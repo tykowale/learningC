bf2f6a713a3de2ea5729e363d0d2a908e5e55798
tykowale <tykowale@gmail.com>
2015-11-05 19:38:19 -0600

    example 5.6

---
+++ ProgrammingInC/chapter5/ex6.c
@@ -0,0 +1,16 @@
+/*Program to introduce the while statement*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int count = 1;
+
+    while (count <= 5) {
+        printf ("%i\n", count);
+        ++count;
+    }
+
+    return 0;
+}
+
