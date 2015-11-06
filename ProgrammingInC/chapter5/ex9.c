5b2b46cbe042d77a2465919194f65c88d6681d9e
tykowale <tykowale@gmail.com>
2015-11-05 20:42:46 -0600

    example 5.9

---
+++ ProgrammingInC/chapter5/ex9.c
@@ -0,0 +1,23 @@
+/*Program to reverse the digits of a number*/
+
+#include <stdio.h>
+
+int main (void)
+{
+    int number, right_digit;
+
+    printf ("Enter your number.\n");
+    scanf ("%i", &number);
+
+    do {
+        right_digit = number % 10;
+        printf ("%i", right_digit);
+        number /= 10;
+    }
+    while ( number != 0 );
+
+    printf("\n");
+
+    return 0;
+}
+
