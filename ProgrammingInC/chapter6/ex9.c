9d6272e0e0034a2506b395eb94fe063d91cbcd07
tykowale <tykowale@gmail.com>
2015-11-07 10:19:09 -0600

    example 6.9

---
+++ ProgrammingInC/chapter6/ex9.c
@@ -0,0 +1,39 @@
+/*Program to evaluate simple expressions of the form */
+/*value operator value*/
+
+#include <stdio.h>
+
+int main (void)
+{
+    float value1, value2;
+    char operator;
+
+    printf ("Type in your expression.\n");
+    scanf ("%f %c %f", &value1, &operator, &value2);
+
+    switch (operator)
+    {
+        case '+':
+            printf ("%.2f\n", value1 + value2);
+            break;
+        case '-':
+            printf ("%.2f\n", value1 - value2);
+            break;
+        case '*':
+            printf ("%.2f\n", value1 * value2);
+            break;
+        case '/':
+            if ( value2 == 0 ) {
+                printf ("Division by zero not allowed.\n");
+            } else {
+                printf ("%.2f\n", value1 / value2);
+            }
+            break;
+        default:
+            printf ("Unknown operator.\n");
+            break;
+    }
+
+    return 0;
+}
+
