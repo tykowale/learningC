81d38ab8afcfd2e8d604feceb195baa22ddf9d0e
tykowale <tykowale@gmail.com>
2015-11-10 18:18:13 -0600

    example 8.1

---
+++ ProgrammingInC/chapter8/ex1.c
@@ -0,0 +1,15 @@
+/*Writing a function in C*/
+
+#include <stdio.h>
+
+void print_message (void)
+{
+    printf ("Programming is fun.\n");
+}
+
+int main (void)
+{
+    print_message ();
+
+    return 0;
+}
